e40c721bd48a90e4cc63cc16ea14cc21f6c9d941
Reffan Liefde <rl@reffanliefde.nl>
2023-05-26 20:04:42 +0200

    Added Stylelint and Sitemap. Wrote some configurations for them.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
-
+import sitemap from '@astrojs/sitemap'
 import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  site: 'https://reffanliefde.nl',
+  integrations: [sitemap(), preact()],
 })
